deff29abc4f3eb8b13c5664d393526d7a3d16411
ssj9685 <ssj9685@naver.com>
2020-03-26 11:06:50 +0900

    bgsub

--- gesture-recognition/gesture.py
+++ gesture-recognition/gesture.py
@@ -1,19 +1,18 @@
 import cv2
 
 def VideoCapture():
+    bgsub = cv2.createBackgroundSubtractorKNN()
     cap=cv2.VideoCapture(0)
     cap.set(3,320)
     cap.set(4,240)
     
     while True:
-        not_err, frame = cap.read()
+        _, frame = cap.read()
+        cv2.imshow("bgsub",bgsub.apply(frame))
         
-        if not_err:
-            cv2.imshow("images",frame)
-            
-            #break when esc press down
-            if cv2.waitKey(1)&0xFF == 27:
-                break
+        #break when esc press down
+        if cv2.waitKey(1)&0xFF == 27:
+            break
             
     cap.release()
     cv2.destroyAllWindows()
